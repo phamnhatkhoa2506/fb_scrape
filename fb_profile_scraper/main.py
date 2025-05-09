bf9189addc1f0d0b56901376c5d0cde1d58d68ab
phamnhatkhoa2506 <163309786+phamnhatkhoa2506@users.noreply.github.com>
2025-05-09 11:56:04 +0700

    Add files via upload

--- fb_profile_scraper/main.py
+++ fb_profile_scraper/main.py
@@ -1,48 +1,47 @@
+# main.py
 import traceback
-from typing import Any
-
+from flask import Flask, request, jsonify
 from utils import (
     convert_urls,
     scrape_profiles_from_urls,
     upload_json_to_gcs,
 )
 
+app = Flask(__name__)
 
-def crawl_facebook_profiles(request: Any) -> Any:
+@app.route("/", methods=["POST"])
+def crawl_facebook_profiles():
     """
-        Cloud Function: Crawl facebook profiles
+    Cloud Run HTTP entry point to crawl Facebook profiles.
     """
-
     try:
-        # Get urls
         request_json = request.get_json(silent=True)
 
         if not request_json or "urls" not in request_json:
-            return "Key 'urls' not in the request body", 400
+            return jsonify(error="Key 'urls' not in the request body"), 400
 
         urls = request_json["urls"]
         if not isinstance(urls, list) or not all(isinstance(u, str) for u in urls):
-            return "'urls' must be the list of string", 400
+            return jsonify(error="'urls' must be a list of strings"), 400
 
         try:
             batch_size = int(request_json.get("batch_size", 2))
         except ValueError:
-            return "'batch_size' must be an integer", 400
+            return jsonify(error="'batch_size' must be an integer"), 400
 
-        print(f"Starting to scrape {len(urls)} user, batch size: {batch_size}")
+        print(f"Starting to scrape {len(urls)} user(s), batch size: {batch_size}")
 
-        urls = convert_urls(urls) # Converst urls to apify form
-        data = scrape_profiles_from_urls(urls, batch_size) # Run scraping
-        gcs_path = upload_json_to_gcs("influencer-profile", data) # Upload to gs
+        urls = convert_urls(urls)
+        data = scrape_profiles_from_urls(urls, batch_size)
+        gcs_path = upload_json_to_gcs("influencer-profile", data)
 
         msg = f"Finished. Upload JSON to: {gcs_path}"
         print(msg)
-        
-        return msg, 200
+
+        return jsonify(message=msg), 200
 
     except Exception as e:
         err_msg = f"Error: {str(e)}"
         print(err_msg)
         traceback.print_exc()
-
-        return err_msg, 500
+        return jsonify(error=err_msg), 500
